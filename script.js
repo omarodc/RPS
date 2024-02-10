5c5ff01021773993173f96349069521e2f626f04
omar sanchez <2490065+omarodc@users.noreply.github.com>
2024-02-10 15:52:33 -0600

    remove 5 game loop

--- script.js
+++ script.js
@@ -51,17 +51,17 @@ function game() {
   let scorePlayer = 0;
   let scoreComputer = 0;
   console.log("Welcome");
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log("/*/*/*/*/*/*/*/*/*/*/*/*");
-    if (checkWinner(playerSelection, computerSelection) == "Player") {
-      scorePlayer++;
-    } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
-      scoreComputer++;
-    }
-  }
+  // for (let i = 0; i < 5; i++) {
+  //   const playerSelection = getPlayerChoice();
+  //   const computerSelection = getComputerChoice();
+  //   console.log(playRound(playerSelection, computerSelection));
+  //   console.log("/*/*/*/*/*/*/*/*/*/*/*/*");
+  //   if (checkWinner(playerSelection, computerSelection) == "Player") {
+  //     scorePlayer++;
+  //   } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
+  //     scoreComputer++;
+  //   }
+  // }
   console.log("Game Over");
   if (scorePlayer > scoreComputer) {
     console.log(`Player was the winner:${scorePlayer} wins`);
